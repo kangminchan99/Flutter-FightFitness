60948b51729cca2ffb53c1becaec3fab9ca404e1
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-03-06 17:32:41 +0900

    2. 로그인 초기 화면 디자인 완료

--- fightfitness/lib/main.dart
+++ fightfitness/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:fightfitness/screen/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_template.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'],
+    javaScriptAppKey: dotenv.env['KAKAO_JAVASCRIPT_KEY'],
+  );
   runApp(const MyApp());
 }
 
--- fightfitness/lib/screen/login_screen.dart
+++ fightfitness/lib/screen/login_screen.dart
@@ -13,7 +13,79 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('login screen'),
+        title: const Text(
+          'FIGHT Fitness',
+          style: TextStyle(
+              fontFamily: 'Jamsil6', color: Colors.white, fontSize: 30),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/gif/main_gif.gif'),
+                fit: BoxFit.fill)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 3,
+              width: MediaQuery.of(context).size.width,
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint('click login apple');
+              },
+              child: Container(
+                width: 300,
+                height: 46,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 7),
+                      child: Image.asset(
+                        'assets/images/Apple_logo.png',
+                        scale: 37,
+                      ),
+                    ),
+                    const Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 15),
+                            child: Text(
+                              '애플 로그인',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                debugPrint('click login kakao');
+              },
+              child: SizedBox(
+                width: 300,
+                height: 46,
+                child: Image.asset('assets/images/kakao_login_large_wide.png'),
+              ),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
